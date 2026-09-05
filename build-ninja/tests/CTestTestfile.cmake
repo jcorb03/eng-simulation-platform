a4fa62c78880bf07cdb94171410c3ba9b86521fb
Joel <Joel@DESKTOP-JU58QE0>
2026-09-05 11:56:56 +0100

    github workflows

---
+++ build-ninja/tests/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/Joel/Documents/Visual Studio 2022/Projects/simulation-platform/tests
+# Build directory: C:/Users/Joel/Documents/Visual Studio 2022/Projects/simulation-platform/build-ninja/tests
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+include("C:/Users/Joel/Documents/Visual Studio 2022/Projects/simulation-platform/build-ninja/tests/test_example_e3b0c442_include.cmake")
--- src/simulation-platform.cpp
+++ src/simulation-platform.cpp
@@ -3,10 +3,8 @@
 
 #include "simulation-platform.h"
 
-using namespace std;
-
 int main()
 {
-    cout << "Hello CMake." << endl;
+    std::cout << "Hello CMake.\n";
     return 0;
 }
